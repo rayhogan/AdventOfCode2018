4790c03e634cb76a7fb9d2cafc16d0bc4a2ddcee
Ray Hogan <rayhogan@gmail.com>
2018-12-01 20:56:32 +0000

    Added integer parsing test

--- AdventOfCode2018.Tests/Day1.Tests.cs
+++ AdventOfCode2018.Tests/Day1.Tests.cs
@@ -33,5 +33,27 @@ namespace AdventOfCode2018.Tests
             // Assert
             Assert.AreEqual(2, freq);
         }
+
+        [TestMethod]
+        public void TestIntegerParsing()
+        {
+            // Arrange
+            string value1 = "-34";
+            string value2 = "+12";
+            string value3 = "0";
+            string value4 = "-1";
+
+            // Act
+            int parsed1 = Day1.ParseIntegerValue(value1);
+            int parsed2 = Day1.ParseIntegerValue(value2);
+            int parsed3 = Day1.ParseIntegerValue(value3);
+            int parsed4 = Day1.ParseIntegerValue(value4);
+
+            // Assert
+            Assert.AreEqual(-34, parsed1);
+            Assert.AreEqual(12, parsed2);
+            Assert.AreEqual(0, parsed3);
+            Assert.AreEqual(-1, parsed4);
+        }
     }
 }
